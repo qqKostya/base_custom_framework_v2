58a915cffa38a278832e72d219d1c60b5443e57c
qqKazakov <leap74rus@gmail.com>
2023-11-18 12:17:56 +0300

    add: tmp for footer, header and sidevar

---
+++ incs/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="p-3 bg-black text-white text-center">
+        &copy; Copyright <?= date('Y'); ?>
+    </div>
+</footer>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
