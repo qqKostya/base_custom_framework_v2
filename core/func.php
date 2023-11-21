4724ccb3b76ee3a092c9e7efd2e1e47630982545
qqKazakov <leap74rus@gmail.com>
2023-11-21 08:03:38 +0300

    add: routing and add get query in htaccess

--- core/func.php
+++ core/func.php
@@ -12,3 +12,9 @@ function dd($data)
     dump($data);
     die;
 }
+
+function abort($code = 404) {
+    http_response_code($code);
+    require_once VIEWS . "/errors/{$code}.tpl.php";
+    die;
+}
--- public/index.php
+++ public/index.php
@@ -10,13 +10,15 @@ define("PATH", 'http://localhost:8888');
 
 require_once CORE . '/func.php';
 
-$uri = trim($_SERVER['REQUEST_URI'], '/');
+$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
 
 if ($uri === '') {
     require_once CONTROLLERS . '/index.php';
-} elseif ($uri === 'about.php') {
+} elseif ($uri === 'about') {
     require_once CONTROLLERS . '/about.php';
+}elseif ($uri === 'post') {
+    dd('SHOW POST');
 } else {
-    require_once VIEWS . '/errors/404.tpl.php';
+    abort();
 }
 
