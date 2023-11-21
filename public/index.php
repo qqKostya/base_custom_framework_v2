6273bbc310307484b64ecb92e43da701e4fd0f08
qqKazakov <leap74rus@gmail.com>
2023-11-21 08:24:43 +0300

    add: routes and fix front controller

--- public/index.php
+++ public/index.php
@@ -1,24 +1,5 @@
 <?php
 
-define("ROOT", dirname(__DIR__));
-define("PUBLIC", ROOT . '/public');
-define("CORE", ROOT . '/core');
-define("APP", ROOT . '/app');
-define("CONTROLLERS", APP . '/controllers');
-define("VIEWS", APP . '/views');
-define("PATH", 'http://localhost:8888');
-
+require_once dirname(__DIR__) . '/config/config.php';
 require_once CORE . '/func.php';
-
-$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
-
-if ($uri === '') {
-    require_once CONTROLLERS . '/index.php';
-} elseif ($uri === 'about') {
-    require_once CONTROLLERS . '/about.php';
-}elseif ($uri === 'post') {
-    dd('SHOW POST');
-} else {
-    abort();
-}
-
+require_once CORE . '/router.php';
