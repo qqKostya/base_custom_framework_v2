5b9d509938602e9a634d7b9f3eaf2220d37c008a
qqKazakov <leap74rus@gmail.com>
2023-11-25 14:05:43 +0300

    add: class Validation

--- app/controllers/posts-create.php
+++ app/controllers/posts-create.php
@@ -4,24 +4,51 @@
  * @var Db $db
  */
 
+require_once CORE . '/classes/Validator.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $fillable = ['title', 'excerpt', 'content'];
     $data = load($fillable);
 
     // validation
-    $errors = [];
-    if (empty($data['title'])) {
-        $errors['title'] = 'Title is required';
+    $validator = new Validator();
+    $validation = $validator->validate($data, [
+        'title' => [
+            'required' => true,
+            'min' => 5,
+            'max' => 190
+        ],
+        'excerpt' => [
+            'required' => true,
+            'min' => 10,
+            'max' => 190
+        ],
+        'content' => [
+            'required' => true,
+            'min' => 100,
+        ]
+    ]);
+
+    if ($validation->hasErrors()) {
+        print_arr($validation->getErrors());
+    } else {
+        echo 'SUCCESS';
     }
 
-    if (empty($data['excerpt'])) {
-        $errors['excerpt'] = 'Excerpt is required';
-    }
 
-    if (empty($data['content'])) {
-        $errors['content'] = 'Content is required';
-    }
+    die;
+//    if (empty($data['title'])) {
+//        $errors['title'] = 'Title is required';
+//    }
+//
+//    if (empty($data['excerpt'])) {
+//        $errors['excerpt'] = 'Excerpt is required';
+//    }
+//
+//    if (empty($data['content'])) {
+//        $errors['content'] = 'Content is required';
+//    }
 
     if (empty($errors)) {
         if ($db->query("INSERT INTO post (`title`, `excerpt`, `content`) VALUES (:title, :excerpt, :content)", $data)) {
---
+++ core/classes/Validator.php
@@ -0,0 +1,75 @@
+<?php
+
+class Validator
+{
+    protected array $errors = [];
+    protected array $rules_list = ['required', 'min', 'max', 'email'];
+    protected array $messages = [
+        'required' => 'The :fieldname: field is required',
+        'min' => 'The :fieldname: field must be a minimun :rulevalue: characters',
+        'max' => 'The :fieldname: field must be a maximum :rulevalue: characters',
+        'email' => 'Not valid email',
+    ];
+
+    public function validate(array $data = [], array $rules = [])
+    {
+        foreach ($data as $fieldname => $value) {
+            if (in_array($fieldname, array_keys($rules))) {
+                $this->check([
+                    'fieldname' => $fieldname,
+                    'value' => $value,
+                    'rules' => $rules[$fieldname],
+                ]);
+            }
+        }
+        return $this;
+    }
+
+    protected function check(array $field)
+    {
+        foreach ($field['rules'] as $rule => $rule_value) {
+            if (in_array($rule, $this->rules_list)) {
+                if (!call_user_func_array([$this, $rule], [$field['value'], $rule_value])) {
+                    $this->addError($field['fieldname'], str_replace([':fieldname:', ':rulevalue:'],
+                        [$field['fieldname'], $rule_value],
+                        $this->messages[$rule]));
+                }
+            }
+        }
+    }
+
+    protected function addError($fieldname, $error)
+    {
+        return $this->errors[$fieldname][] = $error;
+    }
+
+    public function hasErrors()
+    {
+        return !empty($this->errors);
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    protected function required($value, $rule_value)
+    {
+        return !empty(trim($value));
+    }
+
+    protected function min($value, $rule_value)
+    {
+        return mb_strlen($value, 'UTF-8') >= $rule_value;
+    }
+
+    protected function max($value, $rule_value)
+    {
+        return mb_strlen($value, 'UTF-8') <= $rule_value;
+    }
+
+    protected function email($value, $rule_value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+}
