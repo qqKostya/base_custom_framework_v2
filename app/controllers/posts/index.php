376e9aec0e01bbdf7ad9fd15a653e735254f3a73
qqKazakov <leap74rus@gmail.com>
2023-12-04 08:24:05 +0300

    add: start realization class Pagination

--- app/controllers/posts/index.php
+++ app/controllers/posts/index.php
@@ -10,6 +10,17 @@ $title = 'My blog :: Home';
 
 $db = \myfrm\App::get(Db::class);
 
+
+$page = $_GET['page'] ?? 1;
+$per_page = 2;
+$total = $db->query('SELECT COUNT(*) FROM post')->getColumn();
+$pagination = new \myfrm\Pagination((int)$page, $per_page, $total);
+print_arr($pagination);
+
+$start = $pagination->getStart();
+dump($pagination->uri);
+
+die();
 $per_page = 3;
 $total = $db->query('SELECT COUNT(*) FROM post')->getColumn();
 $pages_cnt = ceil($total / $per_page);
